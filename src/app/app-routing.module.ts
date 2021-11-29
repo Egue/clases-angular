7ea0d1d2f59d7d9cf144a381706e45bde6fa4259
intinalambrico <web@internetinalambrico.com.co>
2021-11-29 17:31:43 -0500

    29-tarde

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'supergiros', loadChildren: () => import('./pages/supergiros/supergiros.module').then(m => m.SupergirosModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   {path: '**' , redirectTo: '', pathMatch:'full'}
 ];
 
